cbfbeb929506bb33cadb6b23d92d9dccd4b94a2a
Yuya Hanai <yuya@rhizomatiks.com>
2015-06-12 18:03:19 +0900

    add some helper funcs

--- src/ofxCvCameraUtil.h
+++ src/ofxCvCameraUtil.h
@@ -22,17 +22,35 @@ namespace ofxCvCameraUtil
                   intrinsics.getImageSize().height, outcam);
     }
     
-    static inline cv::Mat createCameraMatrix(const ofCamera& cam, float width, float height) {
-        Intr intr = getIntrinsics(cam, width, height);
-        
+    static inline cv::Mat createCameraMatrix(float cx, float cy, float f) {
         cv::Mat camera_matrix = cv::Mat::zeros(3, 3, CV_64F);
         
-        camera_matrix.ptr<double>()[0] = intr.f;//fx
-        camera_matrix.ptr<double>()[4] = intr.f;//fy
-        camera_matrix.ptr<double>()[2] = intr.cx;//cx
-        camera_matrix.ptr<double>()[5] = intr.cy;//cy
+        camera_matrix.ptr<double>()[0] = f;
+        camera_matrix.ptr<double>()[4] = f;
+        camera_matrix.ptr<double>()[2] = cx;
+        camera_matrix.ptr<double>()[5] = cy;
         camera_matrix.ptr<double>()[8] = 1.0;
         
         return camera_matrix;
+    }
+    
+    static inline cv::Mat createDistCoeffs(float k1, float k2, float p1, float p2, float k3) {
+        cv::Mat dist_coeffs = cv::Mat::zeros(5, 1, CV_64F);
+        dist_coeffs.ptr<double>()[0] = k1;
+        dist_coeffs.ptr<double>()[1] = k2;
+        dist_coeffs.ptr<double>()[2] = p1;
+        dist_coeffs.ptr<double>()[3] = p2;
+        dist_coeffs.ptr<double>()[4] = k3;
+        return dist_coeffs;
+    }
+    
+    static inline cv::Mat createCameraMatrix(const ofCamera& cam, float width, float height) {
+        Intr intr = getIntrinsics(cam, width, height);
+        return createCameraMatrix(intr.cx, intr.cy, intr.f);
+    }
+    
+    static inline void createIntrinsics(const ofCamera& cam, float width, float height, ofxCv::Intrinsics& out_intrinsics)
+    {
+        out_intrinsics.setup(createCameraMatrix(cam, width, height), cv::Size(width, height));
     }
 };

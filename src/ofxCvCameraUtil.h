b9fb8a56ef02776a326e0bca78efbeb2a19b75c2
Yuya Hanai <yuya@rhizomatiks.com>
2015-06-12 09:33:45 +0900

    add some utility functions that does not depend on ofxCv

--- src/ofxCvCameraUtil.h
+++ src/ofxCvCameraUtil.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include "ofxCv.h"
+#include "ofxCvCameraUtilSimple.h"
 
 namespace ofxCvCameraUtil
 {
@@ -12,14 +13,7 @@ namespace ofxCvCameraUtil
         const float cx = camera_matrix.at<double>(0, 2);
         const float cy = camera_matrix.at<double>(1, 2);
         const float fy = camera_matrix.at<double>(1, 1);
-        float fov = ofRadToDeg(2.0f * atanf((h * 0.5f) / fy));
-        
-        outcam.setFov(fov);
-        
-        float lx = -2.0 * (cx - w * 0.5 - 0.5) / w;
-        float ly = 2.0 * (cy - h * 0.5 - 0.5) / h;
-        
-        outcam.setLensOffset(ofVec2f(lx, ly));
+        setCamera(cx, cy, fy, w, h, outcam);
     }
     
     static inline void setCamera(const ofxCv::Intrinsics& intrinsics, ofCamera& outcam)
@@ -29,14 +23,14 @@ namespace ofxCvCameraUtil
     }
     
     static inline cv::Mat createCameraMatrix(const ofCamera& cam, float width, float height) {
+        Intr intr = getIntrinsics(cam, width, height);
+        
         cv::Mat camera_matrix = cv::Mat::zeros(3, 3, CV_64F);
         
-        ofxCv::Intrinsics intr;
-        float focal = 0.5 * height / tanf(ofDegToRad(0.5 * cam.getFov()));
-        camera_matrix.ptr<double>()[0] = focal;//fx
-        camera_matrix.ptr<double>()[4] = focal;//fy
-        camera_matrix.ptr<double>()[2] = 0.5 * width - 0.5;//cx
-        camera_matrix.ptr<double>()[5] = 0.5 * height - 0.5;//cy
+        camera_matrix.ptr<double>()[0] = intr.f;//fx
+        camera_matrix.ptr<double>()[4] = intr.f;//fy
+        camera_matrix.ptr<double>()[2] = intr.cx;//cx
+        camera_matrix.ptr<double>()[5] = intr.cy;//cy
         camera_matrix.ptr<double>()[8] = 1.0;
         
         return camera_matrix;
---
+++ src/ofxCvCameraUtilSimple.h
@@ -0,0 +1,116 @@
+//
+//  Created by Yuya Hanai, https://github.com/hanasaan/
+//
+#pragma once
+
+#include "ofMain.h"
+
+// using without ofxCv.
+namespace ofxCvCameraUtil
+{
+    struct Intr
+    {
+        float cx;
+        float cy;
+        float f;
+        float w;
+        float h;
+        Intr() : cx(0), cy(0), f(0), w(0), h(0) {}
+        Intr(float cx, float cy, float f, float w, float h)
+            : cx(cx), cy(cy), f(f), w(w), h(h) {}
+    };
+    
+    inline ostream& operator<<(ostream& os, const Intr& intr) {
+        int w = 8;
+        int p = 8;
+        os << fixed << setw(w) << setprecision(p) << intr.cx << std::endl;
+        os << intr.cy << std::endl;
+        os << intr.f << std::endl;
+        os << intr.w << std::endl;
+        os << intr.h << std::endl;
+        
+        return os;
+    }
+    
+    inline istream& operator>>(istream& is, Intr& intr) {
+        is >> intr.cx; is.ignore(1);
+        is >> intr.cy; is.ignore(1);
+        is >> intr.f; is.ignore(1);
+        is >> intr.w; is.ignore(1);
+        is >> intr.h;
+        return is;
+    }
+    
+    static inline void setCamera(const Intr& intr, ofCamera& outcam)
+    {
+        float fov = ofRadToDeg(2.0f * atanf((intr.h * 0.5f) / intr.f));
+        
+        outcam.setFov(fov);
+        
+        float lx = -2.0 * (intr.cx - (intr.w * 0.5 - 0.5)) / intr.w;
+        float ly = 2.0 * (intr.cy - (intr.h * 0.5 - 0.5)) / intr.h;
+        
+        outcam.setLensOffset(ofVec2f(lx, ly));
+    }
+
+    static inline void setCamera(float cx, float cy, float fy, float w, float h, ofCamera& outcam)
+    {
+        setCamera(Intr(cx, cy, fy, w, h), outcam);
+    }
+    
+    static inline Intr getIntrinsics(const ofCamera& cam, float width, float height) {
+        float lx = cam.getLensOffset().x;
+        float ly = -cam.getLensOffset().y;
+        
+        float focal = 0.5 * height / tanf(ofDegToRad(0.5 * cam.getFov()));
+        float cx = 0.5 * width * (1.0 - lx) - 0.5;
+        float cy = 0.5 * height * (1.0 - ly) - 0.5;
+        
+        return Intr(cx, cy, focal, width, height);
+    }
+    
+    static void saveIntr(string path, const Intr& intr)
+    {
+        ofFile file(path, ofFile::WriteOnly);
+        file << "#ofxCvCameraUtil::Intr" << endl;
+        file << intr << endl;
+        file.close();
+    }
+    
+    static Intr loadIntr(string path)
+    {
+        Intr intr;
+        ofFile file(path);
+        if (!file.exists()) return Intr();
+        string tmp;
+        file >> tmp;
+        file >> intr;
+        return intr;
+    }
+    
+    static void loadIntrToCamera(string path, ofCamera& outcam)
+    {
+        setCamera(loadIntr(path), outcam);
+    }
+    
+    // some utils
+    static void saveTransform(string path, const ofMatrix4x4& mat, string tag = "#transform")
+    {
+        ofFile file(path, ofFile::WriteOnly);
+        file << tag << endl;
+        file << mat;
+        file << endl;
+        file.close();
+    }
+    
+    static ofMatrix4x4 loadTransform(string path)
+    {
+        ofMatrix4x4 mat;
+        ofFile file(path);
+        if (!file.exists()) return ofMatrix4x4();
+        string tmp;
+        file >> tmp;
+        file >> mat;
+        return mat;
+    }
+};
